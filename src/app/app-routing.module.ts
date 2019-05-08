870c4425d0086357220ff674d8d77929e0fde906
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-05-09 02:01:27 +0300

    added contacts component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,18 +9,20 @@ import { Cab20WComponent } from './body-part/warmehaus/cab20-w/cab20-w.component
 import { FilmsComponent } from './body-part/warmehaus/films/films.component';
 import { AnitIcingComponent } from './body-part/warmehaus/anit-icing/anit-icing.component';
 import { TermostatsComponent } from './body-part/warmehaus/termostats/termostats.component';
+import { ContactsComponent } from './body-part/warmehaus/contacts/contacts.component';
 
 const routes: Routes = [
-   { path: 'home', component: StartPageComponent},
-  // // { path: 'contacts', component: ContactsComponent },
-      { path: 'warmehaus/mat-160Watt', component: WarmingMat160WComponent },
-      { path: 'warmehaus/mat-200Watt', component: WarmingMat200WComponent },
-      { path: 'warmehaus/cable-11Watt', component: Cab11WComponent },
-      { path: 'warmehaus/cable-14Watt', component: Cab14WComponent },
-      { path: 'warmehaus/cable-20Watt', component: Cab20WComponent },
-      { path: 'warmehaus/films', component: FilmsComponent },
-      { path: 'warmehaus/anti-icing', component: AnitIcingComponent },
-      { path: 'warmehaus/termostats', component: TermostatsComponent },
+  { path: 'home', component: StartPageComponent },
+  { path: 'contacts', component: ContactsComponent },
+  { path: 'warmehaus/mat-160Watt', component: WarmingMat160WComponent },
+  { path: 'warmehaus/mat-200Watt', component: WarmingMat200WComponent },
+  { path: 'warmehaus/cable-11Watt', component: Cab11WComponent },
+  { path: 'warmehaus/cable-14Watt', component: Cab14WComponent },
+  { path: 'warmehaus/cable-20Watt', component: Cab20WComponent },
+  { path: 'warmehaus/films', component: FilmsComponent },
+  { path: 'warmehaus/anti-icing', component: AnitIcingComponent },
+  { path: 'warmehaus/termostats', component: TermostatsComponent },
+
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'warmehaus', redirectTo: 'home', pathMatch: 'full' },
 ];
