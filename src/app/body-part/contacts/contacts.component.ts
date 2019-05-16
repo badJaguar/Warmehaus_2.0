d9f1f154dd20abc323b6bd9b8af520414d573471
Aleksandr Ataakgayev <bad.bad.jaguar@gmail.com>
2019-05-16 22:13:37 +0300

    jquery lib added and open hours part

--- src/app/body-part/contacts/contacts.component.ts
+++ src/app/body-part/contacts/contacts.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-contacts',
@@ -14,9 +15,13 @@ export class ContactsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function () {
+      $('.opening-hours li').eq(new Date().getDay()).addClass('today');
+      });
   }
  processForm() {
     const allInfo = `My name is ${this.name}. My email is ${this.email}. My message is ${this.message}`;
     alert(allInfo);
   }
 }
+
