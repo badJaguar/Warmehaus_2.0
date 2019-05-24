411895b74a0cd3e34501cd697ce9c5fddd2dfdc3
aleksandr_ataakgayev <aleksandr_ataakgayev@epam.com>
2019-05-24 16:48:10 +0300

    test xml

--- src/models/IItem.interface.ts
+++ src/models/IItem.interface.ts
@@ -1,4 +1,14 @@
+
 export interface IItem {
+/* A main model for displaying data in table.
+Can be used for displaying complex data in one general table.
+*/
+/*
+For example:
+Warmehaus / mats = (only mats data)
+Warmehaus / termostats = (only termostats data)
+Warmehaus = (data of termostats and mats)
+*/
     id: number;
     name: string;
     nominal: string;
