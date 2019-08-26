e21d6156da26b80d5eeafb3b4df78b3ee9fbc848
aleksandr_ataakgayev <aleksandr_ataakgayev@epam.com>
2019-08-26 16:14:12 +0300

    Instructions filled

--- src/app/body-part/info/info.component.ts
+++ src/app/body-part/info/info.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   templateUrl: './info.component.html',
-  styleUrls: ['./info.component.scss']
+  styleUrls: ['./info.component.scss'],
+  providers: [
+    { provide: Window, useValue: window }
+  ]
 })
 export class InfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(Window) private window: Window) { }
 
   ngOnInit() {
   }
-
+  onTabMenu() {
+    this.window.document.getElementById('tabMenu').scrollIntoView();
+  }
 }
